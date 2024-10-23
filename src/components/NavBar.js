67576e397df3f9f1df5e030ab2519045d87372b8
Shane Fox <foxshane997@gmail.com>
2024-10-22 19:04:39 -0700

    needed to refactor navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,10 @@ const NavBar = () => {
     return (
         <nav>
             <ul className="nav-links">
-                <li><Link to="/#/todo">To Do</Link></li>
-                <li><Link to="/#/counter">Counter</Link></li>
-                <li><Link to="/#/timer">Timer</Link></li>
-                <li><Link to="/#/calculator">Calculator</Link></li>
+                <li><Link to="/todo">To Do</Link></li>
+                <li><Link to="/counter">Counter</Link></li>
+                <li><Link to="/timer">Timer</Link></li>
+                <li><Link to="/calculator">Calculator</Link></li>
             </ul>
         </nav>
     );
